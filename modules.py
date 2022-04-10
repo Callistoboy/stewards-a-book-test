a5c92cca84f3b9c8cbf424ac05ef42c1a56c35bf
Artem <56426715+Callistoboy@users.noreply.github.com>
2022-04-11 01:50:17 +0300

    Update modules.py

--- modules.py
+++ modules.py
@@ -6,4 +6,9 @@ def define_env(env):
     @env.macro
     def redis_get(key):
         r = redis.from_url('redis://:pd3c25ab01ee13171d13997a7c7bd6a1d74834d8dd56fc432467259aaf4dfb257@ec2-34-252-236-53.eu-west-1.compute.amazonaws.com:706')
-        return json.loads(r.get(key))
+        try:
+            response = json.loads(r.get(key))
+        except redis.exceptions.TimeoutError:
+            respone = 504
+            
+        return respone
