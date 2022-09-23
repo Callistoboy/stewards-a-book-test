64f775044aa9730405917c8f555bc0fe2661cfb9
Artem <styleforyou.djart@gmail.com>
2022-09-23 21:50:05 +0300

    допы

--- modules.py
+++ modules.py
@@ -5,5 +5,5 @@ def define_env(env):
     
     @env.macro
     def redis_get(key):
-        r = redis.from_url('redis://:pf887f38265ccb526c53e7d41f972dd08d144ad058e0a6cbc8b50d478dc544041@ec2-52-209-147-40.eu-west-1.compute.amazonaws.com:22259')
+        r = redis.from_url('redis://:pf887f38265ccb526c53e7d41f972dd08d144ad058e0a6cbc8b50d478dc544041@ec2-54-216-133-59.eu-west-1.compute.amazonaws.com:20089')
         return json.loads(r.get(key))
