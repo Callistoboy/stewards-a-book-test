2f2a69fbd07a0dbaa54be98527005c3668de08d3
Artem <styleforyou.djart@gmail.com>
2023-03-19 20:24:25 +0300

    Update modules.py

--- modules.py
+++ modules.py
@@ -8,5 +8,5 @@ def define_env(env):
         r = redis.Redis(host="redis-12618.c250.eu-central-1-1.ec2.cloud.redislabs.com", port=12618, password="f2Y8Bot3CLK0eH7a1aqqNmnlBP4CcFfP")
         return json.loads(r.get(key))
 
-    env.variables.overall_ste = redis_get("a_stewards_count")
-    env.variables.avg_ste = redis_get("a_stewards_avg_count")
+    env.variables.overall_ste = redis_get("a_stewards_count")[0]
+    env.variables.avg_ste = redis_get("a_stewards_avg_count")[0]
